47028f5a6c550c714bcadc08b028e6ba2119f2ef
Елена Николаева <elena@192.168.0.10>
2022-09-26 18:32:51 +0300

    seminar_07/

---
+++ seminar_07/Phone_book/inp.py
@@ -0,0 +1,15 @@
+def mod():
+    mod = input('Выберите режим: 1 - экспорт, 2 - импорт: ')
+    return mod
+
+
+def inpp():
+    str_person = input('Введите все данные через пробел: ')
+    return str_person
+
+
+def exp():
+    second_name = input('Введите фамилию человека: ')
+    return second_name
+
+    
